438f33d73673e383798d7339829c7534e7b8dad1
Sugito <Sugito@Surface-GJ>
2015-10-21 01:03:10 +0700

    remove unused

--- src/main/java/com/tkbaru/web/DashboardController.java
+++ src/main/java/com/tkbaru/web/DashboardController.java
@@ -10,7 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.tkbaru.model.Function;
 import com.tkbaru.model.LoginContext;
 
 @Controller
