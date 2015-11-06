2950bebeef2a6f1e4b477ff4a8fdcac98ceb86c8
gitzjoey <gitzjoey@live.com>
2015-11-06 09:46:36 +0700

    remove unused

--- src/main/java/com/tkbaru/model/LoginContext.java
+++ src/main/java/com/tkbaru/model/LoginContext.java
@@ -17,8 +17,7 @@ public class LoginContext implements Serializable {
 	}
 	
 	private User userLogin;
-	private String selectedMenu;	
-	private String selectedLanguage;
+
 	private List<PurchaseOrder> poList = new ArrayList<PurchaseOrder>();
 	@Valid
 	private List<SalesOrder> soList = new ArrayList<SalesOrder>();
@@ -30,22 +29,6 @@ public class LoginContext implements Serializable {
 
 	public void setUserLogin(User userLogin) {
 		this.userLogin = userLogin;
-	}
-
-	public String getSelectedMenu() {
-		return selectedMenu;
-	}
-
-	public void setSelectedMenu(String selectedMenu) {
-		this.selectedMenu = selectedMenu;
-	}
-
-	public String getSelectedLanguage() {
-		return selectedLanguage;
-	}
-
-	public void setSelectedLanguage(String selectedLanguage) {
-		this.selectedLanguage = selectedLanguage;
 	}
 
 	public List<PurchaseOrder> getPoList() {
@@ -66,9 +49,8 @@ public class LoginContext implements Serializable {
 
 	@Override
 	public String toString() {
-		return "LoginContext [userLogin=" + userLogin + ", selectedMenu="
-				+ selectedMenu + ", selectedLanguage=" + selectedLanguage + "poList Size: " + poList.size() + "soList Size: " + soList.size() + "]";
+		return "LoginContext [userLogin=" + userLogin + ", poList=" + poList + ", soList=" + soList + "]";
 	}
-	
+
 }
 
