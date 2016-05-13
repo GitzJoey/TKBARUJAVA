12c9e7675fd2c7c8779ad5937d01137cdeda2feb
GitzJoey <GitzJoey@DESKTOP-GJ>
2016-05-14 02:07:24 +0700

    trial run fix

--- src/main/java/com/tkbaru/web/DashboardController.java
+++ src/main/java/com/tkbaru/web/DashboardController.java
@@ -1,7 +1,10 @@
 package com.tkbaru.web;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,9 +13,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.tkbaru.model.LoginContext;
 import com.tkbaru.model.Price;
+import com.tkbaru.model.chart.FlotBarChart;
 import com.tkbaru.service.PriceService;
 import com.tkbaru.service.PurchaseOrderService;
 import com.tkbaru.service.SalesOrderService;
@@ -49,11 +54,37 @@ public class DashboardController {
 	}
 	
 	@RequestMapping(value = "/get/sales", method = RequestMethod.GET)
-	public String retrieveSalesData(Locale locale, Model model) {
+	public @ResponseBody FlotBarChart retrieveSalesData(Locale locale, Model model) {
 		logger.info("[retrieveSalesData] " + "Locale: " + locale.toString());
-
 		
-		return "dashboard";
+		FlotBarChart f = new FlotBarChart();
+
+		Long[] arrLong = new Long[] { Long.valueOf("1355040000000"), (long) 2000};
+		
+		
+		ArrayList y = new ArrayList();
+		ArrayList x = new ArrayList();
+		x.add(arrLong);
+		
+		y.add(x);
+		f.setData(y);
+		
+		/*
+		
+		HashMap<String, String> p2 = new HashMap<String, String>();
+		p2.put("1355040000000", "2000");
+		//p.put("1355223600000", "3000");
+		//p.put("1355306400000", "4000");
+		//p.put("1355487300000", "5000");
+		//p.put("1355571900000", "6000");
+		ArrayList s = new ArrayList();
+		
+		s.add(p);
+		s.add(p2);
+		f.setData2(s);
+		*/
+		
+		return f;
 	}
 	
 }
